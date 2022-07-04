6ea576986dd6703fb942b878da0fa4d40cbb21c1
Debarnab2021 <92086998+Debarnab2021@users.noreply.github.com>
2022-07-04 15:46:12 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCqo_rZxIfmXpuCw1_zVqf7evQ7XnWL3lQ",
+    authDomain: "social-media-457e2.firebaseapp.com",
+    databaseURL: "https://social-media-457e2-default-rtdb.firebaseio.com",
+    projectId: "social-media-457e2",
+    storageBucket: "social-media-457e2.appspot.com",
+    messagingSenderId: "594111103877",
+    appId: "1:594111103877:web:a82994f0461a3015537aec",
+    measurementId: "G-9PWEWK973W"
+  };
+
+    firebase.initializeApp(firebaseConfig);
+    var analytics = getAnalytics(app);
